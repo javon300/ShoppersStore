0374a144173d92d0a66b700b65f8ee44145e3b2c
matthew maxwell <matthewmaxwell300@yahoo.com>
2022-04-19 14:21:00 -0600

    read from firebase fixed

--- app/src/main/java/com/mm/shoppersstore/Home.java
+++ app/src/main/java/com/mm/shoppersstore/Home.java
@@ -1,24 +1,52 @@
 package com.mm.shoppersstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Home extends AppCompatActivity
 {
-
+    private static final String TAG = "HOME CLASS";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     Button startButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        readFromDatabase();
     }
 
+    public void readFromDatabase()
+    {
+        db.collection("Products")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
 
 
 }
