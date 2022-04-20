6837234dc39ec2f22f489f82e4655467e9e876a2
Aishwarya Shrestha <aishwaryashrestha4064@gmail.com>
2022-04-19 22:20:49 -0400

    Added btn

--- app/src/main/java/com/mm/shoppersstore/Home.java
+++ app/src/main/java/com/mm/shoppersstore/Home.java
@@ -21,8 +21,28 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+
 public class Home extends AppCompatActivity
 {
+
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+
+    TextView StatusBlueTv, PairedTv;
+    ImageView BlueTv;
+    Button OnBtn, OffBtn, DiscoverBtn, PairedBtn;
+
+    BluetoothAdapter BlueAdapter;
+
+
     private static final String TAG = "HOME CLASS";
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     Button startButton;
@@ -37,9 +57,109 @@ public class Home extends AppCompatActivity
         readFromDatabase();
 
 
+        StatusBlueTv = findViewById(R.id.statusBluetoothTv);
+        PairedTv = findViewById(R.id.pairedTv);
+        BlueTv = findViewById(R.id.bluetoothTv);
+        OnBtn = findViewById(R.id.onBtn);
+        OffBtn = findViewById(R.id.offBtn);
+        DiscoverBtn = findViewById(R.id.discoverBtn);
+        PairedBtn = findViewById(R.id.pairedBtn);
+
+        BlueAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (BlueAdapter == null) {
+            StatusBlueTv.setText("Bluetooth is not available");
+        } else{
+            StatusBlueTv.setText("Bluetooth is available");
+        }
+
+        if(BlueAdapter.isEnabled()) {
+            BlueTv.setImageResource(R.drawable.ic_action_on);
+        } else {
+            BlueTv.setImageResource(R.drawable.ic_action_off);
+        }
+
+        OnBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                if (!BlueAdapter.isEnabled()) {
+                    showToast("Turning On Bluetooth");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(intent, REQUEST_ENABLE_BT);
+                }
+
+                else {
+                    showToast("Bluetooth is already on");
+                }
+            }
+        });
+
+        OffBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick (View v){
+                if (BlueAdapter.isEnabled()) {
+                    BlueAdapter.disable();
+                    showToast("Turning Bluetooth Off");
+                    BlueTv.setImageResource(R.drawable.ic_action_off);
+                }
+                else {
+                    showToast("Bluetooth is already off");
+                }
+            }
+        });
+
+        DiscoverBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick (View v){
+                if (!BlueAdapter.isDiscovering()) {
+                    showToast("Making Your Device Discoverable");
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(intent, REQUEST_DISCOVER_BT);
+                }
+            }
+        });
+
+        PairedBtn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick (View v){
+                if (BlueAdapter.isEnabled()) {
+                    PairedTv.setText("Paired Devices");
+                    Set<BluetoothDevice> devices = BlueAdapter.getBondedDevices();
+                    for (BluetoothDevice device: devices) {
+                        PairedTv.append("\nDevice:" + device.getName() + "," + device);
+                    }
+                }
+                else {
+                    showToast("Turn on bluetooth to get paired devices");
+                }
+            }
+        });
 
 
     }
+
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == RESULT_OK) {
+                    BlueTv.setImageResource(R.drawable.ic_action_on);
+                    showToast("Bluetooth is on");
+                }
+
+                else {
+                    showToast("Couldn't on Bluetooth");
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void showToast(String msg) {
+        Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
+    }
+
+
+
     public void rc(List<Product> data ){
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         ProductRecycleViewAdapter adapter = new ProductRecycleViewAdapter(data, getApplication());
